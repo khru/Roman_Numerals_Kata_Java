7ff1b18dbd6677fd1c5dad6c2717e5c51739174a
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-04-28 17:35:56 +0200

    REFACTOR: remove imports not used

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -1,7 +1,5 @@
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConverterTest {
