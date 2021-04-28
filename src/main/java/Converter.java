aed8dcd79ca3d3418b81545232a75165211c49cb
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-04-28 17:47:56 +0200

    GREEN: updating logic to convert 4 into IV

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,5 +1,8 @@
 public class Converter {
     public String convert(int number) {
+        if (number == 4) {
+            return "IV";
+        }
         if (number == 3) {
           return "III";
         } else if (number == 2) {
