f4f5e05f68c4acc6278c2885fed3ed4e3184e22e
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-04-28 17:44:28 +0200

    REFACTOR: update test so are simpler

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -1,24 +1,28 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConverterTest {
+    private Converter converter;
+
+    @BeforeEach
+    private void init() {
+        this.converter = new Converter();
+    }
 
     @Test
     public void should_convert_1_into_I() {
-        Converter converter = new Converter();
         assertEquals("I", converter.convert(1));
     }
 
     @Test
     public void should_convert_2_into_II() {
-        Converter converter = new Converter();
         assertEquals("II", converter.convert(2));
     }
 
     @Test
     public void should_convert_3_into_III() {
-        Converter converter = new Converter();
         assertEquals("III", converter.convert(3));
     }
 }
