2348cbc35f06036685482859ffb24ed4061bd169
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-04-28 17:43:50 +0200

    GREEN: logic to convert 3 into III

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,8 +1,11 @@
 public class Converter {
     public String convert(int number) {
-        if (number == 2) {
+        if (number == 3) {
+          return "III";
+        } else if (number == 2) {
             return "II";
         }
+
         return "I";
     }
 }
