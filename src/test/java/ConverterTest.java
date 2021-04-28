9536141b206ec84978b422b3515e9efc520f9259
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-04-28 17:28:14 +0200

    REFACTOR: the only test

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -1,18 +1,13 @@
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.Matchers.instanceOf;
 
 class ConverterTest {
 
-	@Test
-	public void give_me_a_good_name_please() {
-		Converter converter = new Converter();
-
-		boolean result = converter.theMethod();
-
-		assertEquals(true, result);
-		assertThat(result, equalTo(true));
-	}
+    @Test
+    public void give_me_a_good_name_please() {
+        Converter converter = new Converter();
+        assertThat(converter, instanceOf(Converter.class));
+    }
 }
