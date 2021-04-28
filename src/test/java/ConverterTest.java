a1c069078893c9161a341054bd39ef5e12f1317d
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-04-28 17:32:50 +0200

    RED: convert method does not exist

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConverterTest {
 
@@ -9,5 +10,11 @@ class ConverterTest {
     public void should_create_a_converter() {
         Converter converter = new Converter();
         assertThat(converter, instanceOf(Converter.class));
+    }
+
+    @Test
+    public void should_convert_1_into_I() {
+        Converter converter = new Converter();
+        assertEquals("I", converter.convert(1));
     }
 }
