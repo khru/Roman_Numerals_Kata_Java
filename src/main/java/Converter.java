58a70a5c2d3199dc201c7f5337f96af4382ef899
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-05-12 21:45:43 +0200

    fix: pre-commit

--- src/main/java/Converter.java
+++ src/main/java/Converter.java
@@ -1,26 +1,26 @@
 public class Converter {
-    public String convert(int number) {
+  public String convert(int number) {
 
-        String romanNumber = "";
-        if (number >= 10) {
-            romanNumber += "X";
-            number -= 10;
-        }
-        if (number == 9) {
-            romanNumber += "IX";
-            number -= 9;
-        } else if (number >= 5) {
-            romanNumber += "V";
-            number -= 5;
-        } else if (number == 4) {
-            romanNumber += "IV";
-            number -= 4;
-        }
-
-        for (int i = 0; i < number ; i++) {
-            romanNumber += "I";
-        }
-
-        return romanNumber;
+    String romanNumber = "";
+    if (number >= 10) {
+      romanNumber += "X";
+      number -= 10;
     }
+    if (number == 9) {
+      romanNumber += "IX";
+      number -= 9;
+    } else if (number >= 5) {
+      romanNumber += "V";
+      number -= 5;
+    } else if (number == 4) {
+      romanNumber += "IV";
+      number -= 4;
+    }
+
+    for (int i = 0; i < number; i++) {
+      romanNumber += "I";
+    }
+
+    return romanNumber;
+  }
 }
