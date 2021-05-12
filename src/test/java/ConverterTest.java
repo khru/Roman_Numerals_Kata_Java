9b6eb1135a613972878104a6da567b6ddee61fcf
Emmanuel Valverde Ramos <evrtrabajo@gmail.com>
2021-05-12 16:43:56 +0200

    pre-commit-test

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -1,63 +1,63 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 class ConverterTest {
-    private Converter converter;
+  private Converter converter;
 
-    @BeforeEach
-    private void init() {
-        this.converter = new Converter();
-    }
+  @BeforeEach
+  private void init() {
+    this.converter = new Converter();
+  }
 
-    @Test
-    public void should_convert_1_into_I() {
-        assertEquals("I", converter.convert(1));
-    }
+  @Test
+  public void should_convert_1_into_I() {
+    assertEquals("I", converter.convert(1));
+  }
 
-    @Test
-    public void should_convert_2_into_II() {
-        assertEquals("II", converter.convert(2));
-    }
+  @Test
+  public void should_convert_2_into_II() {
+    assertEquals("II", converter.convert(2));
+  }
 
-    @Test
-    public void should_convert_3_into_III() {
-        assertEquals("III", converter.convert(3));
-    }
+  @Test
+  public void should_convert_3_into_III() {
+    assertEquals("III", converter.convert(3));
+  }
 
-    @Test
-    public void should_convert_4_into_IV() {
-        assertEquals("IV", converter.convert(4));
-    }
+  @Test
+  public void should_convert_4_into_IV() {
+    assertEquals("IV", converter.convert(4));
+  }
 
-    @Test
-    public void should_convert_5_into_V() {
-        assertEquals("V", converter.convert(5));
-    }
+  @Test
+  public void should_convert_5_into_V() {
+    assertEquals("V", converter.convert(5));
+  }
 
-    @Test
-    public void should_convert_6_into_VI() {
-        assertEquals("VI", converter.convert(6));
-    }
+  @Test
+  public void should_convert_6_into_VI() {
+    assertEquals("VI", converter.convert(6));
+  }
 
-    @Test
-    public void should_convert_7_into_VII() {
-        assertEquals("VII", converter.convert(7));
-    }
+  @Test
+  public void should_convert_7_into_VII() {
+    assertEquals("VII", converter.convert(7));
+  }
 
-    @Test
-    public void should_convert_8_into_VIII() {
-        assertEquals("VIII", converter.convert(8));
-    }
+  @Test
+  public void should_convert_8_into_VIII() {
+    assertEquals("VIII", converter.convert(8));
+  }
 
-    @Test
-    public void should_convert_9_into_IX() {
-        assertEquals("IX", converter.convert(9));
-    }
+  @Test
+  public void should_convert_9_into_IX() {
+    assertEquals("IX", converter.convert(9));
+  }
 
-    @Test
-    public void should_convert_10_into_XX() {
-        assertEquals("XX", converter.convert(10));
-    }
+  @Test
+  public void should_convert_10_into_XX() {
+    assertEquals("XX", converter.convert(10));
+  }
 }
